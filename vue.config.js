d445fe9dd51ada07e7aa6fe607a890a328656b97
unknown <erhanarda7@gmail.com>
2020-06-24 17:32:02 +0300

    vue deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/shopping-list/' : '/'
-    }
+    publicPath: process.env.NODE_ENV === 'shopping-list'
+      ? '/my-project/'
+      : '/'
+  }
+
+
